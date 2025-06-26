03be77a9c45076f9dc41f305fe2c3f5ad3869bbd
Salaheddine Badri <sabadri@e2r6p13.1337.ma>
2025-06-26 15:10:29 +0100

    parse

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: sabadri <sabadri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 16:25:17 by sabadri           #+#    #+#             */
-/*   Updated: 2025/06/24 20:54:44 by sabadri          ###   ########.fr       */
+/*   Updated: 2025/06/25 18:40:01 by sabadri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 typedef struct s_table
 {
@@ -25,6 +26,13 @@ typedef struct s_table
     int sleep;
     int nmeals;
 }   t_table;
+
+typedef struct s_philo
+{
+    int id;
+    t_table *table;
+    pthread_t thread;
+}   t_philo;
 
 //parse
 int check_value(int ac, char **av);
